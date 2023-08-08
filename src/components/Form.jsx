b7dd814ed45018c56b6987af918ae53a9721605b
ChoSangok <chod1510@naver.com>
2023-08-08 13:46:45 +0900

    feat/textfocus

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,25 +2,30 @@ import { Textarea } from "../style/TextareaStyled";
 import { Button } from "../style/ButtonStyled";
 import { Forms } from "../style/FormStyled";
 import { useState } from "react";
+import { useRef } from "react";
 
 const Form = ({ todoList, setTodoList }) => {
   const [text, setText] = useState("");
+  const focusText = useRef(null);
 
   const changeText = (e) => {
     const newText = e.target.value;
+
     setText(newText);
   };
 
   const addTodo = (e) => {
     e.preventDefault();
     const newTodo = { id: Math.random(), text: text };
+
     setTodoList([...todoList, newTodo]);
     setText("");
+    focusText.current.focus();
   };
 
   return (
     <Forms>
-      <Textarea value={text} onChange={(e) => changeText(e)} />
+      <Textarea value={text} onChange={(e) => changeText(e)} ref={focusText} />
       <Button onClick={(e) => addTodo(e)} type="submit">
         submit
       </Button>
--- src/style/FormStyled.jsx
+++ src/style/FormStyled.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Forms = styled.form`
   display: flex;
+  align-items: center;
   margin: 10px;
-
   width: 40rem;
 `;
